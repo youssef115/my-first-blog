e1e0b6bab3012462ded8240190c09993a51a4fc8
youssef115 <youy15118@gmail.com>
2022-04-21 08:50:58 +0100

    django after post new

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
+
 from django.contrib import admin
 from django.urls import path ,include
 
@@ -20,3 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
 ]
+
